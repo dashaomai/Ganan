a164fe7e6ef035fba086dc7a1760c6b73f75686b
Bob Jiang <dashaomai@gmail.com>
2017-04-25 20:00:54 +0800

    添加了关闭时的行为

--- src/main/scala/ganan/LauncherApp.scala
+++ src/main/scala/ganan/LauncherApp.scala
@@ -9,4 +9,14 @@ import ganan.connector.actors.WebSocketConnector
 object LauncherApp extends App {
   private val system = ActorSystem("ganan")
   system.actorOf(WebSocketConnector.props("127.0.0.1", 9595))
+
+  private val log = system.log
+
+  Runtime.getRuntime.addShutdownHook(new Thread(new Runnable {
+    override def run(): Unit = {
+      log.info("系统即将关闭")
+
+      system.terminate()
+    }
+  }))
 }
